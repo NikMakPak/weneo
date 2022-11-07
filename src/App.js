be8b423a156afa1a136f60d0621de5cbca4aeb69
nikmakpak <nicement28@gmail.com>
2022-11-07 10:41:03 +0300

    feat: add architecture

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import React  from "react";
 import TextAnalize from "./Components/TextAnalize/TextAnalize";
 import BlockSite from "./Components/BlockSite/BlockSite";
-import Bl from "./Components/BlockSite/Bl";
 
 function App() {
   return (<>
           <TextAnalize/>
-      <BlockSite/>
-
+            <BlockSite/>
   </>
   );
 }
